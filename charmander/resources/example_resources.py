a17f5adc25c2a58964796b94ed0eb7605738478a
cadmium <constantine.dm@gmail.com>
2019-08-19 16:04:56 -0400

    Init from cookiecutter.
    We initialize our services from this project:
    
    https://github.com/globality-corp/cookiecutter-microcosm-service
    
    Which uses the python `cookiecutter` package under the hood to autogenerate our
    service boilerplate structure. These include:
    
    - Sample routes, resources, etc
    - Sample postgres models and stores

---
+++ charmander/resources/example_resources.py
@@ -0,0 +1,43 @@
+"""
+Example resources.
+
+"""
+from marshmallow import Schema, fields
+from microcosm_flask.linking import Link, Links
+from microcosm_flask.namespaces import Namespace
+from microcosm_flask.operations import Operation
+from microcosm_flask.paging import PageSchema
+
+from charmander.models.example_model import Example
+
+
+class NewExampleSchema(Schema):
+    name = fields.String(
+        required=True,
+    )
+
+
+class ExampleSchema(NewExampleSchema):
+    id = fields.UUID(
+        required=True,
+    )
+    _links = fields.Method(
+        "get_links",
+        dump_only=True,
+    )
+
+    def get_links(self, obj):
+        links = Links()
+        links["self"] = Link.for_(
+            Operation.Retrieve,
+            Namespace(
+                subject=Example,
+                version="v1",
+            ),
+            example_id=obj.id,
+        )
+        return links.to_dict()
+
+
+class SearchExampleSchema(PageSchema):
+    name = fields.String()
---
+++ setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+from setuptools import find_packages, setup
+
+
+project = "charmander"
+version = "0.1.0"
+
+setup(
+    name=project,
+    version=version,
+    description="Short Project Description",
+    author="Globality Engineering",
+    author_email="engineering@globality.com",
+    url="https://github.com/globality-corp/microcosm-sample-service",
+    packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        "microcosm>=2.12.0",
+        "microcosm-flask[metrics,spooky]>=1.20.0",
+        "microcosm-logging>=1.3.0",
+        "microcosm-postgres>=1.9.1",
+        "microcosm-secretsmanager>=1.1.0",
+        "pyOpenSSL>=18.0.0",
+    ],
+    setup_requires=[
+        "nose>=1.3.7",
+    ],
+    entry_points={
+        "console_scripts": [
+            "createall = charmander.main:createall",
+            "migrate = charmander.main:migrate",
+            "runserver = charmander.main:runserver",
+        ],
+    },
+    extras_require={
+        "test": [
+            "coverage>=4.0.3",
+            "PyHamcrest>=1.9.0",
+        ],
+    },
+)
